bd4611550346829396d1305384ce7b5e99b88bf8
Jake <jakenovin3>
2026-06-12 18:38:48 -0700

    Added styling to the main page. Added state capture for entered job.

--- ResumeReviewer/src/App.jsx
+++ ResumeReviewer/src/App.jsx
@@ -1,23 +1,43 @@
-import React from "react";
-import "./App.css";
+import React, { useState } from "react";
+//import uploadIcon from 'assets/upload.svg'
 
 function PageHeader() {
   return (
     <div id="pageHeaderRoot">
       <div id="pageHeaderMain">
-        <h1>Resume Rx</h1>
+        <div id="titleDesc">
+          <h1>Resume Rx</h1>
+          <p>Advanced feedback in seconds</p>
+        </div>
+        <span>Powered by AI</span>
       </div>
     </div>
   );
 }
 
-function JobSearch() {
+function JobSearch({ setJob }) {
+  const [input, setInput] = useState("");
+
   return (
     <div id="jobSearchRoot">
-      <h3>Step 1 — Job search</h3>
-      <form id="jobSearchForm">
-        <input id="jobSearchInput"></input>
-      </form>
+      <div className="stepHeaders">
+        <div>
+          <b>1</b>
+        </div>
+        <h3>Job search</h3>
+      </div>
+      <div id="jobSearchSection">
+        <form id="jobSearchForm">
+          <input
+            id="jobSearchInput"
+            onChange={(e) => setInput(e.target.value)}
+            placeholder="e.g. Frontend Developer, Phoenix AZ"
+          ></input>
+        </form>
+        <button id="jobSearchSubmit" onClick={() => setJob(input)}>
+          &gt;
+        </button>
+      </div>
     </div>
   );
 }
@@ -25,7 +45,12 @@ function JobSearch() {
 function FileUpload() {
   return (
     <div id="fileUploadRoot">
-      <h3>Step 2 — Upload your resume</h3>
+      <div className="stepHeaders">
+        <div>
+          <b>2</b>
+        </div>
+        <h3>Upload your resume</h3>
+      </div>
       <div id="fileUploadArea">
         <p>Drop PDF or Word Doc here or click to upload</p>
       </div>
@@ -34,15 +59,18 @@ function FileUpload() {
 }
 
 export default function App() {
+  const [job, setJob] = useState("");
+  const [resume, setResume] = useState(null);
+
   return (
     <>
       <div id="headerMain">
         <PageHeader />
       </div>
       <div id="bodyMain">
-        <JobSearch />
+        <JobSearch setJob={setJob} />
         <FileUpload />
-        <button id="submitResumeBtn">Rate</button>
+        <button id="submitResumeBtn">Start reviewing!</button>
       </div>
     </>
   );

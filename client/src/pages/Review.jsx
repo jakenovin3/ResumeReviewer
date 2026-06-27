9d768d31d2ba7cb8f6bd9de339b491aa88f5df73
Jake <jakenovin3>
2026-06-26 19:39:47 -0700

    Reformatting and applied updated stylings to the Review page. Made Review page more mobile friendly. Added template data. Added tooltip when hovering over ratings.

--- client/src/pages/Review.jsx
+++ client/src/pages/Review.jsx
@@ -10,31 +10,53 @@ export default function Review() {
       </div>
       <div id="bodyMain">
         <div className="reviewRatingSection">
-          <div>
-            <h1>Overall: 4.2/5 stars</h1>
-            <div>
-              <p>ATS Pass Likelihood: </p>
-              <span id="atsPassFail">Likely to pass</span>
+          <div id="scores">
+            <h1>Score: 4.2/5 stars</h1>
+            <div id="atsScoreDiv">
+              <p>ATS Score: </p>
+              <p id="atsScoreValue">
+                <b>86</b>
+              </p>
             </div>
           </div>
+          <hr />
           <div id="ratingAreas">
-            <div className="rating">
-              <h3>Relevant Experience</h3>
+            <div className="rating tooltip">
+              <h3>Experience</h3>
               <h2>3.9 stars</h2>
+              <span className="tooltiptext">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy text ever since 1966
+              </span>
             </div>
-            <div className="rating">
+            <div className="vl"></div>
+            <div className="rating tooltip">
               <h3>Skills Match</h3>
               <h2>4.5 stars</h2>
+              <span className="tooltiptext">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy text ever since 1966
+              </span>
             </div>
-            <div className="rating">
-              <h3>Formatting/Clarity</h3>
+            <div className="vl"></div>
+            <div className="rating tooltip">
+              <h3>Clarity</h3>
               <h2>4.1 stars</h2>
+              <span className="tooltiptext">
+                Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem
+                Ipsum has been the industry's standard dummy text ever since 1966
+              </span>
             </div>
           </div>
         </div>
         <div className="reviewSummarySection reviewSection">
-          <h3>Summary</h3>
-          <p></p>
+          <h3>Review Summary</h3>
+          <p>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+            has been the industry's standard dummy text ever since 1966, when designers at Letraset
+            and James Mosley, the librarian at St Bride Printing Library in London, took a 1914
+            Cicero translation and scrambled it to make dummy text for Letraset's Body Type sheets.
+          </p>
         </div>
         <div className="reviewNotesSection reviewSection">
           <div>
